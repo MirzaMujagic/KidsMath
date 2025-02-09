8ce03d0b6ba59d56743ddcd51afc761cbe2c09ad
Mimatic <mirza.mujagic@live.com>
2025-02-09 01:09:13 +0100

    Initial Commit

---
+++ KidsMath/KidsMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsMathApp.swift
+//  KidsMath
+//
+//  Created by Mirza Mujagic on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct KidsMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
